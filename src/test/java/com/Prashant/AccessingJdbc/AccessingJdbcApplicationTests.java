2bbefcded72ff266710cd9900109d5d343592ae4
gauravprsnt <prsnt.gaurav@gmail.com>
2018-05-09 11:01:17 +0545

    latest Commit

---
+++ src/test/java/com/Prashant/AccessingJdbc/AccessingJdbcApplicationTests.java
@@ -0,0 +1,16 @@
+package com.Prashant.AccessingJdbc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AccessingJdbcApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
